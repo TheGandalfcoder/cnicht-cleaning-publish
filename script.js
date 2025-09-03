59e0964d00c20060fa904e536ddd511db784ab34
TheGandalfcoder <j4jos123@gmail.com>
2025-09-03 17:54:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ["./**/*.html", "./**/*.js"], // Adjust to your project
+    darkMode: 'class', // ✅ Required for class-based dark mode
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: 'class', // Enables manual dark mode via 'class'
+  content: ['./**/*.html'], // Adjust this to match your project's HTML or component file paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
